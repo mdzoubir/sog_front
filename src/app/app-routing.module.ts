e92fdb869107ce83785ac9fd6ca81428ab7b2d8e
mdzoubir <57962405+mdzoubir@users.noreply.github.com>
2021-08-08 23:25:29 +0100

    start about component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
+import { AboutComponent } from './component/about/about.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponentComponent } from './component/home/home.component';
 
 const routes: Routes = [
   {path: '',   redirectTo: '/home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponentComponent}
+  {path: 'home', component: HomeComponentComponent},
+  {path: 'about', component: AboutComponent}
 ];
 
 @NgModule({
